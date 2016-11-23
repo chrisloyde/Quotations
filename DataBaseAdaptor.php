1ed9baa5198e7d7c84a8b7c3bc77830042639b9a
mejiasbreanb1 <mejiasbreanb1@email.arizona.edu>
2016-11-22 17:00:34 -0700

    Add files via upload

--- DataBaseAdaptor.php
+++ DataBaseAdaptor.php
@@ -1,63 +1,78 @@
-<?php
-// Author: Chris Peterson
-
-class DatabaseAdaptor {
-    private $DB;
-
-    public function __construct() {
-        $db = 'mysql:dbname=quotes;host=127.0.0.1';
-        $user = 'root';
-        $password = '';
-
-        try {
-            $this->DB = new PDO($db, $user, $password);
-            $this->DB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        } catch (PDOException $e) {
-            echo ('Error establishing connection.');
-            exit();
-        }
-    }
-
-    public function getQuotesAsArray() {
-            $stmt = $this->DB->prepare("SELECT * FROM quotations ORDER BY points DESC, dateAdded DESC");
-            $stmt->execute();
-            return $stmt->fetchAll(PDO::FETCH_ASSOC);
-    }
-
-    public function add($quote, $author) {
-        $stmt = $this->DB->prepare("INSERT INTO quotations (dateAdded, quotation, author, points)
-                                    values(now(), :quote, :author, 0)");
-        $stmt->bindParam('quote', $quote);
-        $stmt->bindParam('author', $author);
-        $stmt->execute();
-    }
-
-    public function delete($quote) {
-        $stmt = $this->DB->prepare("DELETE FROM quotations
-                                    WHERE quotation=:quote");
-        $stmt->bindParam('quote', $quote);
-        $stmt->execute();
-    }
-
-    public function changePoint($id, $value) {
-        $stmt = $this->DB->prepare("SELECT * FROM quotations WHERE ID=" .$id);
-        echo "ID = " . intVal($id) . PHP_EOL;
-        //$stmt->bindParam('id', $id);
-        $stmt->execute();
-        $points = $stmt->fetchColumn(4);
-        echo "VALUE = " . $value. PHP_EOL;
-        $value = $value+intval($points);
-        echo "POINTS = " . intVal($points) . PHP_EOL;
-        echo "VALUE = " . $value. PHP_EOL;
-
-        $stmt = $this->DB->prepare("UPDATE quotations SET points=:value WHERE ID=:id");
-        $stmt->bindParam('value', $value);
-        $stmt->bindParam('id', $id);
-        $stmt->execute();
-    }
-
-    public function sort() {
-        $stmt = $this->DB->prepare("SELECT * FROM quotations ORDER BY points");
-        $stmt->execute();
-    }
-}
+ <?php
+	// Quotes Enhanced: a Dynamic Website that is Part 1 of a final project 
+	// as a final project, except there is no AJAX in this example.
+	//
+	// Author: Rick Mercer and Hassanain Jamal
+	//
+	// TODO: Handle the two new forms for 
+	// registering 
+	// logging in
+	// flagging one quote
+	// unflagging all quotes
+	// logging out
+	//
+	class DatabaseAdaptor {
+		// The instance variable used in every one of the functions in class DatbaseAdaptor
+		private $DB;
+		// Make a connection to an existing data based named 'quotes' that has
+		// table quote. In this assignment you will also need a new table named 'users'
+		public function __construct() {
+			$db = 'mysql:dbname=quotes;host=127.0.0.1';
+			$user = 'root';
+			$password = '';
+			
+			try {
+				$this->DB = new PDO ( $db, $user, $password );
+				$this->DB->setAttribute ( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+			} catch ( PDOException $e ) {
+				echo ('Error establishing Connection');
+				exit ();
+			}
+		}
+		
+		// Return all quote records as an associative array.
+		// Example code to show id and flagged columns of all records:
+		// $myDatabaseFunctions = new DatabaseAdaptor();
+		// $array = $myDatabaseFunctions->getQuotesAsArray();
+		// foreach($array as $record) {
+		// echo $record['id'] . ' ' . $record['flagged'] . PHP_EOL;
+		// }
+		//
+		public function getQuotesAsArray() {
+			// possible values of flagged are 't', 'f';
+			$stmt = $this->DB->prepare ( "SELECT * FROM quotations WHERE flagged='f' ORDER BY rating DESC, added" );
+			$stmt->execute ();
+			return $stmt->fetchAll ( PDO::FETCH_ASSOC );
+		}
+		
+		// Insert a new quote into the database
+		public function addNewQuote($quote, $author) {
+			$stmt = $this->DB->prepare ( "INSERT INTO quotations (added, quote, author, rating, flagged ) values(now(), :quote, :author, 0, 'f')" );
+			$stmt->bindParam ( 'quote', $quote );
+			$stmt->bindParam ( 'author', $author );
+			$stmt->execute ();
+		}
+		
+		// Raise the rating of the quote with the given $ID by 1
+		public function raiseRating($ID) {
+			$stmt = $this->DB->prepare ( "UPDATE quotations SET rating=rating+1 WHERE id= :ID" );
+			$stmt->bindParam ( 'ID', $ID );
+			$stmt->execute ();
+		}
+		
+		// Lower the rating of the quote with the given $ID by 1
+		public function lowerRating($ID) {
+			$stmt = $this->DB->prepare ( "UPDATE quotations SET rating=rating-1 WHERE id= :ID" );
+			$stmt->bindParam ( 'ID', $ID );
+			$stmt->execute ();
+		}
+		
+	} // end class DatabaseAdaptor
+	
+	$myDatabaseFunctions = new DatabaseAdaptor ();
+	
+	// Test code can only be used temporarily here. If kept, deleting account 'fourth' from anywhere would 
+	// cause these asserts to generate error messages. And when did you find out 'fourth' is registered?
+	// assert ( $myDatabaseFunctions->verified ( 'fourth', '4444' ) );
+	// assert ( ! $myDatabaseFunctions->canRegister ( 'fourth' ) );
+	?>
